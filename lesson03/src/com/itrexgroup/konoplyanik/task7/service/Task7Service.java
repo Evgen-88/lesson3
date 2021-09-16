94c9a8c514edbd374a591f91d9388debcf4097db
Konoplyanik Evgeniy <53610766+Evgen-88@users.noreply.github.com>
2021-09-16 15:56:45 +0300

    Update Task7Service.java

--- lesson03/src/com/itrexgroup/konoplyanik/task7/service/Task7Service.java
+++ lesson03/src/com/itrexgroup/konoplyanik/task7/service/Task7Service.java
@@ -10,7 +10,7 @@ public class Task7Service {
 	public static long calcSumOfCanceledTransOnNonEmptyAccounts(List<Account> accounts) {
 		return accounts.stream().filter(acc -> acc.getBalance() > 0).flatMap(acc -> acc.getTransactions().stream())
 				.filter(t -> t.getState().equals(State.CANCELED)).mapToLong(t -> t.getSum())
-				.reduce(0, (sum, s) -> sum + s);
+				.sum();
 	}
 
 }
